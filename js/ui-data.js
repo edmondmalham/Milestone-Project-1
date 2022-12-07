989f8bc22a8af1143b766a7108da1d1afa896b77
Edmond Malham <edmondmalham@Edmonds-MacBook-Pro-2.local>
2022-12-07 14:19:40 -0800

    add UI data file

---
+++ js/ui-data.js
@@ -0,0 +1,73 @@
+const UI_DATA = {
+    rounds: [
+        {
+            title: 'Round One',
+            questions: [
+                {
+                    question: 'What is used to make hummus?',
+                    score: 100,
+                    answers: ['Pinto Beans', 'Chick Peas', 'Sunflower Seeds'],
+                    correctAnswerIndex: 1
+                },
+                {
+                    question: 'What turns a plain martini into a dirty martini?',
+                    score: 100,
+                    answers: ['Gin', 'Vermouth', 'Olive Juice'],
+                    correctAnswerIndex: 2
+                },
+                {
+                    question: 'What type of spice will turn rice yellow?',
+                    score: 100,
+                    answers: ['Saffron', 'Cumin', 'Cayenne'],
+                    correctAnswerIndex: 0
+                },
+            ]
+        },
+        {
+            title: 'Round Two',
+            questions: [
+                {
+                    question: 'Where were French Fries invented?',
+                    score: 200,
+                    answers: ['Belgium', 'France', 'Spain'],
+                    correctAnswerIndex: 0
+                },
+                {
+                    question: 'Where does Caesar Salad originate from?',
+                    score: 200,
+                    answers: ['Rome', 'Athens', 'Tijuana'],
+                    correctAnswerIndex: 2
+                },
+                {
+                    question: 'Where is the fortune cookie originally from?',
+                    score: 200,
+                    answers: ['China', 'Japan', 'USA'],
+                    correctAnswerIndex: 1
+                },
+            ]
+        },
+        {
+            title: 'Final Round',
+            questions: [
+                {
+                    question: 'What was the first food eaten in space?',
+                    score: 300,
+                    answers: ['Apple Sauce', 'Ice Cream', 'Spam'],
+                    correctAnswerIndex: 0
+                },
+                {
+                    question: 'If you suffer from Mageirocophobia, what are you afraid of?',
+                    score: 300,
+                    answers: ['Food', 'Drinking', 'Cooking'],
+                    correctAnswerIndex: 2
+                },
+                {
+                    question: 'What is the most commonly stolen item of food in the world?',
+                    score: 300,
+                    answers: ['Cheese', 'Bread', 'Chocolate'],
+                    correctAnswerIndex: 0
+                },
+            ]
+        },
+    ]
+};
